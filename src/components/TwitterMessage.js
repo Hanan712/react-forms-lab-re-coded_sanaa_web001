448c024246e42af9427fdb2b6a0a8ddf25af13f0
hanan7121993 <hanan7121993@gmail.com>
2020-09-26 17:07:05 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -16,6 +16,8 @@ class TwitterMessage extends React.Component {
      }
 
   render() {
+    let charNumber = this.props.maxChars - this.state.message.length;
+
     return (
       <div>
         <strong>Your message:</strong>
